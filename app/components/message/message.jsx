8a0eeb924c0562a36d17ed771f184b6dcc592662
Fidelis Msacky <fidelismsacky@yahoo.com>
2018-01-07 20:01:45 -0500

    Chat CSS fixes

--- app/components/message/message.jsx
+++ app/components/message/message.jsx
@@ -17,10 +17,35 @@ export default class Message extends React.Component {
             return (
                 <div className="message-list">
                     {this.props.messages.map(function (message, index) {
+                        if (self.props.username === message.author) {
+                            return (
+                                <div className="chat-content user" key={index}>
+                                    <div className="chat-body inline-block">
+                                        <div className="header">
+                                            <strong
+                                                className={"message-author right"}>{message.author}</strong>
+                                            <small
+                                                className={"message-timestamp"}>
+                                                {moment(new Date(message.timestamp)).format('MMM Do YYYY, h:mm a')}
+                                            </small>
+                                        </div>
+                                        <p className="message-content">{message.text}</p>
+                                        <Divider/>
+                                    </div>
+                                    <div className={"chat-head inline-block right"}>
+                                         <span className="chat-img">
+                                                <img
+                                                    src="https://www.themarysue.com/wp-content/uploads/2015/12/avatar.jpeg"
+                                                    className="img-circle user-avatar"/>
+                                         </span>
+                                    </div>
+                                </div>
+                            )
+                        }
+
                         return (
                             <div className="chat-content" key={index}>
-                                <div
-                                    className={(self.props.username === message.author) ? "chat-head inline-block right" : "chat-head inline-block"}>
+                                <div className={"chat-head inline-block"}>
                                      <span className="chat-img">
                                             <img src="https://www.themarysue.com/wp-content/uploads/2015/12/avatar.jpeg"
                                                  className="img-circle user-avatar"/>
@@ -29,9 +54,9 @@ export default class Message extends React.Component {
                                 <div className="chat-body inline-block">
                                     <div className="header">
                                         <strong
-                                            className={(self.props.username === message.author) ? "message-author right" : "message-author"}>{message.author}</strong>
+                                            className={"message-author"}>{message.author}</strong>
                                         <small
-                                            className={(self.props.username === message.author) ? "message-timestamp" : "message-timestamp right"}>
+                                            className={"message-timestamp right"}>
                                             {moment(new Date(message.timestamp)).format('MMM Do YYYY, h:mm a')}
                                         </small>
                                     </div>
